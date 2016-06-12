6a1fd0de9aeb688352e175881caa9ed0f6938a03
Vladimir Cernov <cernov.vladimir@gmail.com>
2016-06-12 23:41:53 +0300

    mainloop: wrap core functionality
    Signed-off-by: Vladimir Cernov <cernov.vladimir@gmail.com>

--- soletta/soletta.go
+++ soletta/soletta.go
@@ -1,16 +1,32 @@
 package soletta
 
-type Soletta struct {
+/*
+#cgo CFLAGS: -I/usr/include/soletta/
+#cgo LDFLAGS: -lsoletta
+#include "soletta.h"
+*/
+import "C"
+
+func Init() bool {
+	r := C.sol_init()
+	if r == 0 {
+		return true
+	}
+	return false
 }
 
-func NewSoletta() *Soletta {
-	return &Soletta{}
+func Run() bool {
+	r := C.sol_run()
+	if r == C.EXIT_SUCCESS {
+		return true
+	}
+	return false
 }
 
-func (s *Soletta) Start() bool {
-	return true
+func Quit() {
+	C.sol_quit()
 }
 
-func (s *Soletta) Stop() bool {
-	return true
+func Shutdown() {
+	C.sol_shutdown()
 }
